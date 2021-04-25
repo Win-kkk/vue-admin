7ea032f5fa6f3d3875c7fe48aa8dfa829b4155fa
kkk <374207807@qq.com>
2021-04-25 18:53:22 +0800

    构建项目、配置全局样式、路由、elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        这里是登录页
+    </div>
+</template>
+<script>
+export default{
+    name:'login'
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh; //可视区的比例
+    background-color: #344a5f;
+}
+</style>
